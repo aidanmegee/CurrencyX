f84d6ef8e706adbb91de79116f557e560332aff0
Aidan <aidan.megee@my.jcu.edu.au>
2020-04-03 16:54:04 +1000

    Add code to change textview depending on currency conversion. Change convert button to textview and add buttons for user that determines what currency is converted to.

--- app/src/main/java/com/example/currencyx/ExchangeActivity.java
+++ app/src/main/java/com/example/currencyx/ExchangeActivity.java
@@ -2,12 +2,13 @@ package com.example.currencyx;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class ExchangeActivity extends AppCompatActivity {
@@ -26,22 +27,101 @@ public class ExchangeActivity extends AppCompatActivity {
 
     }
 
-    public void currencyConversion() { //method for converting currency
-        //code that using current currency exchange values AUD ---> USD 0.62
+    public void audToUSD() { ////AUD --> United States Dollars
         EditText aud = findViewById(R.id.aud);
         aud.setInputType(InputType.TYPE_CLASS_NUMBER); //input type set to number value
+
+        TextView usdConversion = findViewById(R.id.convert);
+        usdConversion.setText("AUD TO USD");
 
         EditText currencyConverted = findViewById(R.id.currency_converted);
         currencyConverted.setInputType(InputType.TYPE_CLASS_NUMBER);
 
         double dollars = Integer.parseInt(aud.getText().toString()); //wraps Integer value to produce the string value of dollars converted
         double result = dollars * USD;
+        currencyConverted.setText(String.valueOf(result)); //returns string value of conversion
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void audToEUR() { //AUD --> EURO POUNDS
+        EditText aud = findViewById(R.id.aud);
+        aud.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        TextView eurConversion = findViewById(R.id.convert);
+        eurConversion.setText("AUD TO EURO");
+
+        EditText currencyConverted = findViewById(R.id.currency_converted);
+        currencyConverted.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        double dollars = Integer.parseInt(aud.getText().toString());
+        double result = dollars * EUR;
         currencyConverted.setText(String.valueOf(result));
     }
 
-    public void convertOnClick(View view) { //On Click method that coverts currency
-        currencyConversion(); //convert method called currencyConversion
+    @SuppressLint("SetTextI18n")
+    public void audToGBP() { //AUD --> Great British Pounds
+        EditText aud = findViewById(R.id.aud);
+        aud.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        TextView gbpConversion = findViewById(R.id.convert);
+        gbpConversion.setText("AUD TO GBP");
+
+        EditText currencyConverted = findViewById(R.id.currency_converted);
+        currencyConverted.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        double dollars = Integer.parseInt(aud.getText().toString());
+        double result = dollars * GBP;
+        currencyConverted.setText(String.valueOf(result));
     }
+
+    @SuppressLint("SetTextI18n")
+    public void audToINR() { //AUD --> EURO Indian Rupee
+        EditText aud = findViewById(R.id.aud);
+        aud.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        TextView inrConversion = findViewById(R.id.convert);
+        inrConversion.setText("AUD TO INR");
+
+        EditText currencyConverted = findViewById(R.id.currency_converted);
+        currencyConverted.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        double dollars = Integer.parseInt(aud.getText().toString());
+        double result = dollars * INR;
+        currencyConverted.setText(String.valueOf(result));
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void audToJPY() { //AUD --> Japanese Yen
+        EditText aud = findViewById(R.id.aud);
+        aud.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        TextView jpyConversion = findViewById(R.id.convert);
+        jpyConversion.setText("AUD TO JPY");
+
+        EditText currencyConverted = findViewById(R.id.currency_converted);
+        currencyConverted.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        double dollars = Integer.parseInt(aud.getText().toString());
+        double result = dollars * JPY;
+        currencyConverted.setText(String.valueOf(result));
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void audToCAD() { //AUD --> Canadian Dollars
+        EditText aud = findViewById(R.id.aud);
+        aud.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        TextView cadConversion = findViewById(R.id.convert);
+        cadConversion.setText("AUD TO CAD");
+
+        EditText currencyConverted = findViewById(R.id.currency_converted);
+        currencyConverted.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        double dollars = Integer.parseInt(aud.getText().toString());
+        double result = dollars * CAD;
+        currencyConverted.setText(String.valueOf(result));
+    }
+
 
     public void resetExchange(View view) { //resets values of edit text elements
         Button reset = findViewById(R.id.reset);
@@ -50,6 +130,34 @@ public class ExchangeActivity extends AppCompatActivity {
         EditText currencyConverted = findViewById(R.id.currency_converted);
         aud.setText("");
         currencyConverted.setText("");
+    }
+
+    public void onButtonClick(View view) {
+        switch (view.getId()) {
+            case (R.id.usd):
+                audToUSD();
+                break;
+
+            case (R.id.eur):
+                audToEUR();
+                break;
+
+            case (R.id.gbp):
+                audToGBP();
+                break;
+
+            case (R.id.inr):
+                audToINR();
+                break;
+
+            case (R.id.jpy):
+                audToJPY();
+                break;
+
+            case (R.id.cad):
+                audToCAD();
+                break;
+        }
     }
 
 }
